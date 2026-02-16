68194bed472dac9fc18b96136f748a4a202014bd
Arda Şimşek <simsekarda278@gmail.com>
2026-02-16 23:16:45 +0300

    aaa

---
+++ sıraalma/sıraalma/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace sıraalma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblM1 = new System.Windows.Forms.TextBox();
+            this.lblM2 = new System.Windows.Forms.TextBox();
+            this.lblM3 = new System.Windows.Forms.TextBox();
+            this.lblM4 = new System.Windows.Forms.TextBox();
+            this.lblM5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(37, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "sıra al";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(54, 211);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 132);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(244, 211);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 132);
+            this.listBox2.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.lblM5);
+            this.groupBox1.Controls.Add(this.lblM4);
+            this.groupBox1.Controls.Add(this.lblM3);
+            this.groupBox1.Controls.Add(this.lblM2);
+            this.groupBox1.Controls.Add(this.lblM1);
+            this.groupBox1.Location = new System.Drawing.Point(497, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(278, 403);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sıradaki";
+            // 
+            // lblM1
+            // 
+            this.lblM1.Location = new System.Drawing.Point(26, 68);
+            this.lblM1.Name = "lblM1";
+            this.lblM1.Size = new System.Drawing.Size(100, 22);
+            this.lblM1.TabIndex = 0;
+            this.lblM1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblM2
+            // 
+            this.lblM2.Location = new System.Drawing.Point(26, 125);
+            this.lblM2.Name = "lblM2";
+            this.lblM2.Size = new System.Drawing.Size(100, 22);
+            this.lblM2.TabIndex = 1;
+            // 
+            // lblM3
+            // 
+            this.lblM3.Location = new System.Drawing.Point(26, 188);
+            this.lblM3.Name = "lblM3";
+            this.lblM3.Size = new System.Drawing.Size(100, 22);
+            this.lblM3.TabIndex = 2;
+            // 
+            // lblM4
+            // 
+            this.lblM4.Location = new System.Drawing.Point(26, 237);
+            this.lblM4.Name = "lblM4";
+            this.lblM4.Size = new System.Drawing.Size(100, 22);
+            this.lblM4.TabIndex = 3;
+            // 
+            // lblM5
+            // 
+            this.lblM5.Location = new System.Drawing.Point(26, 286);
+            this.lblM5.Name = "lblM5";
+            this.lblM5.Size = new System.Drawing.Size(100, 22);
+            this.lblM5.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Info;
+            this.button2.Location = new System.Drawing.Point(183, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "M1";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Info;
+            this.button3.Location = new System.Drawing.Point(183, 125);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "M2";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Info;
+            this.button4.Location = new System.Drawing.Point(183, 188);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "M3";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Info;
+            this.button5.Location = new System.Drawing.Point(183, 236);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "M4";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.Info;
+            this.button6.Location = new System.Drawing.Point(183, 286);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "M5";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox lblM5;
+        private System.Windows.Forms.TextBox lblM4;
+        private System.Windows.Forms.TextBox lblM3;
+        private System.Windows.Forms.TextBox lblM2;
+        private System.Windows.Forms.TextBox lblM1;
+    }
+}
+
